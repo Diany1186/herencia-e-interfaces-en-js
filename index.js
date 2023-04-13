6a8549c0d39efeab4f818c597ec24b40dd1ad2f1
DianyLocal <diany1186@hotmail.com>
2023-04-13 14:30:58 -0500

    creando clase cuenta ahorros

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Definición de clases
 import {Cliente} from "./Cliente.js";
 import {CuentaCorriente} from "./CuentaCorriente.js";
+import {CuentaAhorro } from "./CuentaAhorro.js";
 
 const cliente = new Cliente("Leonardo","1231231231","1231231");
 const cuentaLeonardo = new CuentaCorriente(cliente,"1","001");
@@ -8,11 +9,7 @@ const cuentaLeonardo = new CuentaCorriente(cliente,"1","001");
 const cliente2 = new Cliente("Sofía","1231231232","1231232");
 const cuentaSofia = new CuentaCorriente(cliente2,"2","002");
 
-let saldo = cuentaLeonardo.verSaldo();
-saldo = cuentaLeonardo.depositoEnCuenta(200);
-saldo = cuentaLeonardo.retirarDeCuenta(50);
-console.log(`El saldo actual de Leonardo es ${cuentaLeonardo.verSaldo()}`);
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente, "9985","001",0);
 
-console.log(CuentaCorriente.cantidadCuentas);
-
-
+console.log(cuentaLeonardo);
+console.log(cuentaAhorroLeonardo);
